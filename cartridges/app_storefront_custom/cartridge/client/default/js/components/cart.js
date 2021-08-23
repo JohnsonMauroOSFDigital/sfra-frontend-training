adbe644381b7d0221c343f47d62d3eb3da21e70e
Johnson Mauro <johnson.mauro@osf.digital>
2021-08-23 17:22:18 -0300

    WIP

---
+++ cartridges/app_storefront_custom/cartridge/client/default/js/components/cart.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// $(".open-coupon-code").on("click", function() {
+//     if (!$(".coupon-code-container").hasClass("d-none")) return;
+
+//     $(".open-coupon-container").addClass("d-none");
+//     $(".coupon-code-container").removeClass("d-none");
+// });
