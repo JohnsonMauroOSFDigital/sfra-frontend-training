2d354f4aa9d6e04a41f5450b3592523b40a20613
Johnson Mauro <johnson.mauro@osf.digital>
2021-08-30 15:47:57 -0300

    WIP

--- cartridges/app_storefront_custom/cartridge/client/default/js/components/test.js
+++ cartridges/app_storefront_custom/cartridge/client/default/js/components/test.js
@@ -1,1 +1,14 @@
-console.log('test');
+console.log("test");
+
+$(document).ready(function() {
+    $(".regular").slick({
+        infinite: true,
+        asNavFor: '.regular-sync'
+    });
+
+    $(".regular-sync").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
+});
